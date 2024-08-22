0314ab40d6053e5f04e760ce885b3ec8c71e599a
nuyada <vova-isaev-2015@mail.ru>
2024-08-22 15:56:10 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Collections;
+using System.Diagnostics;
+using System.Text;
+
+
+namespace CountWords
+{
+    class Program
+    {
+        //  Объявим словарь с двумя значениями
+
+
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("C:\\Users\\DALBI\\Desktop\\Text1.txt");
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+            List<string> words = new List<string>(text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries));
+            var stopWatch = Stopwatch.StartNew();
+            List<string> list = new List<string>();
+            foreach (var word in words)
+            {
+                list.Add(word);
+            }
+            stopWatch.Stop();
+            Console.WriteLine($"Время вставки в List<t> {stopWatch.Elapsed.TotalMilliseconds}");
+            stopWatch.Restart();
+            LinkedList<string> linkedlist = new LinkedList<string>();
+            foreach (var word in words)
+            {
+                linkedlist.AddLast(word);
+            }
+            stopWatch.Stop();
+            Console.WriteLine($"Время вставки в LinkedList<t> {stopWatch.Elapsed.TotalMilliseconds}");
+        }
+
+    }
+}
